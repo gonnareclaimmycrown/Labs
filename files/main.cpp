ce28e962e37a72ea66fc0f93433ae3c45100f810
Fiodar Drazhnikau <144004977+gonnareclaimmycrown@users.noreply.github.com>
2023-10-22 21:58:56 +0300

    Add files via upload

--- files/main.cpp
+++ files/main.cpp
@@ -1,72 +1,48 @@
 #include <iostream>
-
+#include <fstream>
+#include <string>
+using namespace std;
 int main() {
-#include <iostream>
-#include <vector>
-    using namespace std;
+    string input_file_name("..\\input.txt");
 
-    int main() {
-        int const size = 20;
-        char cool[size];
-        cout << "enter string" << endl;
-        cin.getline(cool, size);
-        vector <char> DigitWords(size);
-        vector <char> DigitWordsFake(size);
-        vector <char> OtherWords(size);
-        int count = 0, i = 0, bear = 0;
-        bool isDigitWord;
-        while (true){
-            cout << i << " ";
-            int j = count;
-            isDigitWord = true;
-            if (int(cool[i]) < 48 || int(cool[i]) > 57){
-                while(int(cool[i]) != 32 && i < size){
-                    OtherWords.push_back(cool[i]);
-                    i++;
-                    isDigitWord = false;
-                    bear = i;
-                    cout << i << " ";
-                }
-            }
-            else if (int(cool[i]) > 47 && int(cool[i]) < 58){
-                while (int(cool[i]) != 32 && i < size){
-                    DigitWordsFake.push_back(cool[i]);
-                    i++;
-                    if (int(cool[i]) < 48 || int(cool[i]) > 57){
-                        isDigitWord = false;
-                        DigitWordsFake.clear();
-                        while (int(cool[i]) != 32 && i < size){
-                            OtherWords.push_back (cool[bear]);
-                            bear++;
-                            i++;
-                        }
-                    }
-                }
-            }
-            count = i;
-            if (isDigitWord){
-                while(j <= count){
-                    j++;
-                    DigitWords.push_back(cool[j]);
-                }
-            }
-            i++;
-            cout << "xd" << " ";
-            if (i >= size){
-                goto end1;
-            }
-        }
-        end1:
-        int k = 0;
-        while (k < sizeof (cool)){
-            cout << DigitWords[k];
-            k++;
-        }
-        k = 0;
-        while (k < sizeof (cool)){
-            cout << OtherWords[k];
-            k++;
-        }
+    ifstream input_file(input_file_name);
+    if (!input_file) {
+        cout << "File no found." << endl;
         return 0;
     }
 
+    string output_file1_name("..\\output1.txt");
+
+    string output_file2_name("..\\output2.txt");
+
+    ofstream output_file1(output_file1_name);
+    ofstream output_file2(output_file2_name);
+
+    if (!output_file1 || !output_file2) {
+        cout << "Couldn't open the files" << endl;
+        return 0;
+    }
+
+    string line;
+    while (getline(input_file, line)) {
+        int word_count = 0;
+        for (char c : line) {
+            if (c == ' ') {
+                word_count++;
+            }
+        }
+        word_count++;
+
+        if (word_count <= 2) {
+            output_file1 << line << endl;
+        } else if (word_count >= 3 && word_count <= 6) {
+            output_file2 << line << endl;
+        }
+    }
+
+    output_file1.close();
+    output_file2.close();
+    input_file.close();
+
+    return 0;
+}
